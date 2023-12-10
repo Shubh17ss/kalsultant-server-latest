d5ae95a48ac83c5d32a48b8e286ddfa2422b2c16
Shubh17ss <shubhthe2001@gmail.com>
2023-12-10 20:51:05 +0530

    ui changes--> now showing reviews section only on mobile. Added logo in some screens

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
-const cookieParser=require('cookie-parser');
-const path=require('path');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
-const userRoutes=require('./Routes/user');
-const sessionRoutes=require('./Routes/session');
-const slotsRoutes=require('./Routes/slots');
-const adminRoutes=require('./Routes/admin');
-
-
+const userRoutes = require('./Routes/user');
+const sessionRoutes = require('./Routes/session');
+const slotsRoutes = require('./Routes/slots');
+const adminRoutes = require('./Routes/admin');
 //for environment variables
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/user',userRoutes);
-app.use('/api/session',sessionRoutes);
-app.use('/api/slots',slotsRoutes);
-app.use('/api/admin',adminRoutes);
-
+app.use('/api/user', userRoutes);
+app.use('/api/session', sessionRoutes);
+app.use('/api/slots', slotsRoutes);
+app.use('/api/admin', adminRoutes);
 
 
 //code to render the react build folder
-app.use(express.static(path.join(__dirname,"./build")));
+app.use(express.static(path.join(__dirname, "./build")));
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./build/index.html"));
-})
-
-
-
-
-const server=app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
 })
+
+
 
 
 
