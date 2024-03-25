6490151240e44f064b9f724a9f17f1bd214c3468
[Shubh Sharma] <shubhthe2001@gmail.com>
2024-03-25 13:54:26 +0530

    first push to deploy the app in new account

--- Database/connect.js
+++ Database/connect.js
@@ -2,7 +2,7 @@ const {Client} =require('pg');
 
 
 const pool=new Client({
-    connectionString:`postgres://pqlardcvmbatih:de68ede205b67cfd1b1e63e8552bb4a3182722b674962c4b45bb1521ee8ac96e@ec2-35-169-11-108.compute-1.amazonaws.com:5432/d2snae5i64dusi`,
+    connectionString:`postgres://vlhiqjgyfalvsv:2cece9e54f669fa0cfa3e87a56b05c18043ba4c0993a424f17c9f648cac458b0@ec2-34-252-169-131.eu-west-1.compute.amazonaws.com:5432/d5gb3m319k0hg`,
     ssl: {
       rejectUnauthorized: false
     }
