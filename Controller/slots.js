33dd98b0f3b761eaa547be944496bad9c1a819b0
[Shubh Sharma] <shubhthe2001@gmail.com>
2025-03-23 16:18:59 +0530

    Change to get slots api to cap max occupancy of 6 in a day

--- Controller/slots.js
+++ Controller/slots.js
@@ -3,13 +3,29 @@ const pool = require('../Database/connect');
 
 const getSlots = (req, res) => {
     const { date } = req.body;
-    console.log(date);
-    pool.query('SELECT slot from slots where date=$1 AND booked is not true order by slot', [date], (error, results) => {
+    pool.query('SELECT count(id) from sessions where session_date=$1 AND (session_slot is NOT NULL OR proposed_slot is NOT NULL) ', [date], (error, result) => {
         if (error) {
-            res.status(400).send('Internal Server Error');
+            res.status(400).json({ error: 'Internal server error' });
+            return;
         }
         else {
-            res.status(200).json({ results: results.rows });
+            // reached maximum handling capacity for the day
+            let count = result.rows[0].count;
+            if (Number(count) === 6) {
+                res.status(300).json({ message: 'Maximum capacity of 6 slots reached for the day' });
+                return;
+            }
+            else {
+                pool.query('SELECT slot from slots where date=$1 AND booked is not true order by slot', [date], (error, results) => {
+                    if (error) {
+                        res.status(400).send('Internal Server Error');
+                    }
+                    else {
+                        res.status(200).json({ results: results.rows });
+                    }
+                })
+            }
+
         }
     })
 }
